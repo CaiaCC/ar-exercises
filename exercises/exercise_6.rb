00acf29f591ef3518a44816e09bfa443b0a8de43
Caia Chuang <caiachuang@gmail.com>
2020-09-15 18:40:37 -0700

    Change the hourly rate to at least have 40

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -10,8 +10,8 @@ puts "----------"
 
 # Your code goes here ...
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
-@store2.employees.create(first_name: "Caia", last_name: "Chuang", hourly_rate: 20)
-@store1.employees.create(first_name: "Trent", last_name: "You", hourly_rate: 40)
+@store2.employees.create(first_name: "Caia", last_name: "Chuang", hourly_rate: 40)
+@store1.employees.create(first_name: "Trent", last_name: "You", hourly_rate: 60)
 @store2.employees.create(first_name: "James P.", last_name: "Sullivan", hourly_rate: 90)
 @store1.employees.create(first_name: "Trent", last_name: "You", hourly_rate: 40)
 @store2.employees.create(first_name: "James P.", last_name: "Sullivan", hourly_rate: 90)
