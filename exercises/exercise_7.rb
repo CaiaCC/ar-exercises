6a016822568d65badb6c369f4662f2ad39072416
Caia Chuang <caiachuang@gmail.com>
2020-09-15 18:41:44 -0700

    Add validations to two models and add error meg to validate

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,26 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+# class Employee < ApplicationRecord
+#   validates :first_name, :last_name, :store_id, present: true
+#   validates :hourly_rate, numericality: {only_integer: true, less_than_or_equal_to: 200, greater_than_or_equal_to: 40}
+# end
+
+# class Store < ApplicationRecord
+#   validates :name, length: {minimum: 3}
+#   validates :annual_revenue, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+#   validate :must_carry_at_least_one_apparel
+
+#   def must_carry_at_least_one_apparel
+#     if !mens_apparel && !womens_apparel
+#       errors.add(:mens_apparel, :womens_apparel, "Must carry at least one of the men's or women's apparel")
+#     end
+#   end
+# end
+
+print "Input a store name"
+print ">"
+new_store_name = gets.chomp
+new_store = Store.create(name: new_store_name )
+new_store.valid?
+puts new_store.errors.messages
--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,6 @@
 class Employee < ActiveRecord::Base
-  belongs_to :store
+  belongs_to :store, required: true
+  validates :first_name, :last_name, presence: true
+  # validates :last_name,  present: true
+  validates :hourly_rate, numericality: {only_integer: true, less_than_or_equal_to: 200, greater_than_or_equal_to: 40}
 end
